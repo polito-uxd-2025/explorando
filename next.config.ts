868a8d07f1f380afa601b8232f1d28c3ab09b165
Simone Aronica <40305701+xNicklaj@users.noreply.github.com>
2026-01-06 16:38:55 +0100

    Not sure what I'm doing

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  basePath: process.env.GITHUB_ACTIONS ? '/explorando' : '',
   /* config options here */
 };
 
