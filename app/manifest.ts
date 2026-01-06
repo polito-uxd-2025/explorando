0a99d2f259333be01c1316b292078984a2ddd9f7
Simone Aronica <40305701+xNicklaj@users.noreply.github.com>
2026-01-06 18:19:57 +0100

    Come on workers

--- app/manifest.ts
+++ app/manifest.ts
@@ -16,13 +16,13 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#000000',
     icons: [
       {
-        src: `${basePath}/icon-192x192.png`,
+        src: `${basePath || '/'}/icon-192x192.png`,
         sizes: '192x192',
         type: 'image/png',
         purpose: 'any',
       },
       {
-        src: `${basePath}/icon-512x512.png`,
+        src: `${basePath || '/'}/icon-512x512.png`,
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any',
