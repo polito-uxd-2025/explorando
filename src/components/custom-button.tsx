dd28a8956e6cff7de96f2ad01a26eacc4411145c
Simone Aronica <40305701+xNicklaj@users.noreply.github.com>
2026-01-11 17:19:51 +0100

    Updated button text

--- src/components/custom-button.tsx
+++ src/components/custom-button.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
 
 export const Button = ({ className, children, href }: { className?: string, children: React.ReactNode, href?: string }) => {
-    return <Link href={href || "#"} className={className || ""}><button className={`flex flex-row items-center gap-2 bg-accent-500 p-3 rounded-4xl justify-center w-full`}>{children}</button></Link>;
+    return <Link href={href || "#"} className={className || ""}><button className={`flex flex-row items-center gap-2 bg-accent-500 p-3 rounded-4xl justify-center w-full text-white`}>{children}</button></Link>;
 };
