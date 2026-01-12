d80f79378aeede4a8827a1f29edea819889837ac
Simone Aronica <40305701+xNicklaj@users.noreply.github.com>
2026-01-12 01:40:08 +0100

    Fixed map not loading

--- src/components/map-content.tsx
+++ src/components/map-content.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 interface MapContentProps {
     activityData: any;
@@ -11,22 +12,26 @@ export function MapContent({ activityData }: MapContentProps) {
     const longitude = activityData?.Location?.longitude || -0.09;
     const title = activityData?.Title || 'Activity';
 
+    console.log('Map rendering with:', { latitude, longitude, title, activityData });
+
     return (
-        <MapContainer 
-            center={[latitude, longitude]} 
-            zoom={13} 
-            scrollWheelZoom={false}
-            style={{ height: '100vh', width: '100%' }}
-        >
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={[latitude, longitude]}>
-                <Popup>
-                    {title}
-                </Popup>
-            </Marker>
-        </MapContainer>
+        <div className="w-full h-screen">
+            <MapContainer 
+                center={[latitude, longitude]} 
+                zoom={13} 
+                scrollWheelZoom={false}
+                className="w-full h-full"
+            >
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[latitude, longitude]}>
+                    <Popup>
+                        {title}
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
     );
 }
